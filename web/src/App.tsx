8de818592b1aae40af4388bcc6bf74c7797f5804
Nau Nau <nauanycc@outlook.com.br>
2021-05-04 22:13:24 -0300

    refactor: change app into functional component and modify html color

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">Wow, header!</header>
-      <div>Not a header though.</div>
-    </div>
-  );
-}
+import * as React from "react";
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">Wow, header!</header>
+    <div>Not a header though.</div>
+  </div>
+);
 
 export default App;
