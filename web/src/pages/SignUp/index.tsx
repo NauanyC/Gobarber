796c1a33480c7e1b11149108e82f564a73d04ecf
Nau Nau <nauanycc@outlook.com.br>
2021-05-06 14:17:54 -0300

    feat: errors validation with YUP

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import * as React from "react";
+import React, { useCallback } from "react";
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Form } from "@unform/web";
+import * as Yup from "yup";
 import { Container, Content, Background } from "./styles";
 import logoImg from "../../assets/logo.svg";
 
@@ -8,9 +9,23 @@ import Button from "../../components/Button";
 import Input from "../../components/Input";
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("Email obrigatório")
+          .email("Digite um email válido."),
+        password: Yup.string().min(6, "No mínimo seis dígitos."),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
