1c2f897ccbb99c139bd93eef604680d87cdcbe39
Nau Nau <nauanycc@outlook.com.br>
2021-05-06 14:04:18 -0300

    feat: style according to focus and filling

--- web/src/components/Input/styles.ts
+++ web/src/components/Input/styles.ts
@@ -1,18 +1,37 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+interface InputContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<InputContainerProps>`
   display: flex;
   align-items: center;
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
   color: #666360;
+  border: 2px solid #232129;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     color: #f4ede8;
