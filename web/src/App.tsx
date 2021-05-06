eb381a269915b7c64d47c2b6087ddf9177905e97
Nau Nau <nauanycc@outlook.com.br>
2021-05-06 12:15:28 -0300

    feat: add signUp page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import GlobalStyles from "./styles/global";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App: React.FC = () => (
   <div className="App">
     <GlobalStyles />
     <SignIn />
+    <SignUp />
   </div>
 );
 
